3648606d6bc03b8806724870673f7edfc1f69eae
Mathias Dannesbo <neic@neic.dk>
2013-03-25 03:52:03 +0100

    basic Reference model

--- ddweb_app/models.py
+++ ddweb_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Reference(models.Model):
+    ship = models.CharField(max_length=200)
+    owner = models.CharField(max_length=200)
+    place = models.CharField(max_length=200)
+    constructionType = models.CharField(max_length=1, choices=(('n', 'New Building',),('r','Refurbishment',),), default='r')
+    year = models.PositiveIntegerField(blank=True, null=True)
+    description = models.CharField(max_length=10000)
+    projectManager = models.CharField(max_length=200)
+    projectAssistant = models.CharField(max_length=200)
+    onsiteManager = models.CharField(max_length=200)
+    ongoing = models.BooleanField()
+    beforeDD = models.BooleanField()
+
+    def __unicode__(self):
+        if self.year:
+            return self.ship + " " + str(self.year)
+        else:
+            return self.ship
