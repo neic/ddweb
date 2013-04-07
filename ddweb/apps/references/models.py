27d49dabe4937a5bf02b725a9a9182b24cb2aee4
Mathias Dannesbo <neic@neic.dk>
2013-04-07 22:38:46 +0200

    Manager choices

--- ddweb/apps/references/models.py
+++ ddweb/apps/references/models.py
@@ -1,15 +1,36 @@
 from django.db import models
 
 class Reference(models.Model):
+    CBH = 'CBH'
+    TIK = 'TIK'
+    HG = 'HG'
+    CBHTIK = 'CBT'
+    TIKHG = 'TIH'
+    MANAGER_CHOICES = (
+        (CBH, 'Claus B. Hansen'),
+        (TIK, 'Tina Kjeldgaard'),
+        (HG, 'Henrik Graahede'),
+        (CBHTIK, 'Claus B. Hansen / Tina Kjeldaard'),
+        (TIKHG, 'Tina Kjeldgaard / Henrik Graahede')
+    )
+
     ship = models.CharField(max_length=200)
     owner = models.CharField(max_length=200)
     place = models.CharField(max_length=200)
-    constructionType = models.CharField(max_length=1, choices=(('n', 'New Building',),('r','Refurbishment',),), default='r')
+    constructionType = models.CharField(max_length=1,
+                                        choices=(('n', 'New Building',),('r','Refurbishment',),),
+                                        default='r')
     year = models.PositiveIntegerField(blank=True, null=True)
-    description = models.CharField(max_length=10000)
-    projectManager = models.CharField(max_length=200)
-    projectAssistant = models.CharField(max_length=200)
-    onsiteManager = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    projectManager = models.CharField(max_length=3,
+                                      choices=MANAGER_CHOICES,
+                                      blank=True)
+    projectAssistant = models.CharField(max_length=3,
+                                      choices=MANAGER_CHOICES,
+                                      blank=True)
+    onsiteManager = models.CharField(max_length=3,
+                                      choices=MANAGER_CHOICES,
+                                      blank=True)
     ongoing = models.BooleanField()
     beforeDD = models.BooleanField()
 
