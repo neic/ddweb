1ad67b06526cf0beb01d95ed28e7c269dbc5195d
Mathias Dannesbo <neic@neic.dk>
2015-01-24 15:07:36 +0100

    Move upload urls to /news

--- ddweb/apps/news/urls.py
+++ ddweb/apps/news/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import patterns, url
 from ddweb.apps.news import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+                       url(r'^$', 'ddweb.apps.news.views.news',
+                           name='news'),
+                       url(r'^upload/', 'ddweb.apps.news.views.upload',
+                           name = 'jfu_upload' ),
+                       url(r'^delete/(?P<pk>\d+)$', 'ddweb.apps.news.views.upload_delete',
+                           name = 'jfu_delete' ),
+                       url(r'^uploadf/(?P<article_id>\w+)', 'ddweb.apps.news.views.newsImgUpload',
+                           name = 'uploadform' ),
+
 )
