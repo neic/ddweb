28f5c8b73a2a988191ba7463bb944471c84dc91d
Mathias Dannesbo <neic@neic.dk>
2015-05-12 15:02:28 +0200

    Remove unused imports

--- ddweb/apps/references/views.py
+++ ddweb/apps/references/views.py
@@ -1,11 +1,6 @@
 import os
-from django.conf import settings
-from django.contrib.auth.decorators import permission_required
-from django.core.urlresolvers import reverse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import generic
-from django.views.decorators.http import require_POST
-from jfu.http import upload_receive, UploadResponse, JFUResponse
 
 from ddweb.apps.references.models import Reference
 
