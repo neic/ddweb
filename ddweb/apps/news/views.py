5daf463deba9c89079aec025d541318763d0f969
Mathias Dannesbo <neic@neic.dk>
2015-07-16 15:22:05 +0200

    Remove inner query from news

--- ddweb/apps/news/views.py
+++ ddweb/apps/news/views.py
@@ -3,8 +3,7 @@ from django.views.generic.dates import YearArchiveView, ArchiveIndexView
 from ddweb.apps.news.models import Article
 
 class NewsLatest(ArchiveIndexView):
-    inner_q = Article.objects.all().values('pk')[0:20]
-    queryset = Article.objects.filter(pk__in=inner_q)
+    queryset = Article.objects.all().order_by('date')
     date_field = "date"
     make_object_list = True
     context_object_name = "articles"
