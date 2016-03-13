42fbe3d698a13013d1f846510ad518ff5b9d0062
Mathias Dannesbo <neic@neic.dk>
2016-03-13 17:39:25 -0500

    Replace imagekit with sorl-thumbnail: apps/news

--- ddweb/apps/news/urls.py
+++ ddweb/apps/news/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
-from ddweb.apps.news.views import NewsLatest, NewsArchive
+from ddweb.apps.news.views import NewsArchive
 
 urlpatterns = patterns('',
-                       url(r'^$', NewsLatest.as_view(),
+                       url(r'^$', 'ddweb.apps.news.views.newsLatest',
                            name='news'),
                        url(r'^(?P<year>\d{4})/$', NewsArchive.as_view(),
                            name='news_archive'))
--- ddweb/apps/news/views.py
+++ ddweb/apps/news/views.py
@@ -1,13 +1,11 @@
 from django.views.generic.dates import YearArchiveView, ArchiveIndexView
+from django.shortcuts import render
 
 from ddweb.apps.news.models import Article
 
-class NewsLatest(ArchiveIndexView):
-    queryset = Article.objects.all().order_by('date')
-    date_field = "date"
-    make_object_list = True
-    context_object_name = "articles"
-    template_name = 'news.html'
+def newsLatest(request, **kwargs):
+    context = {'articles': Article.objects.all().order_by('date')}
+    return render(request, 'news.html', context)
 
 class NewsArchive(YearArchiveView):
     queryset = Article.objects.all()
