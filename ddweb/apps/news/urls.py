b684dbc3a1931528d563a26c3129a8bef6247d8e
Mathias Dannesbo <neic@neic.dk>
2018-09-03 13:40:57 +0200

    Remove Article from news

--- ddweb/apps/news/urls.py
+++ ddweb/apps/news/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 
-from ddweb.apps.news.views import newsLatest, NewsArchive
+from ddweb.apps.news.views import newsLatest
 
 urlpatterns = [
     url(r'^$', newsLatest,
         name='news'),
-    url(r'^(?P<year>\d{4})/$', NewsArchive.as_view(),
-        name='news_archive')
     ]
--- ddweb/apps/news/views.py
+++ ddweb/apps/news/views.py
@@ -1,15 +1,5 @@
-from django.views.generic.dates import YearArchiveView, ArchiveIndexView
 from django.shortcuts import render
 
-from ddweb.apps.news.models import Article
-
 def newsLatest(request, **kwargs):
-    context = {'articles': Article.objects.all()}
+    context = {}
     return render(request, 'news.html', context)
-
-class NewsArchive(YearArchiveView):
-    queryset = Article.objects.all()
-    date_field = "date"
-    make_object_list = True
-    context_object_name = "articles"
-    template_name = 'news.html'
