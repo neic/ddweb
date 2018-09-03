02e3d965a3c5e2bfcdb7f381024d8db2b690f78b
Mathias Dannesbo <neic@neic.dk>
2018-09-03 15:17:33 +0200

    Remove unnecessary news/views.py

--- ddweb/apps/news/urls.py
+++ ddweb/apps/news/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
-from ddweb.apps.news.views import newsLatest
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', newsLatest,
+    url(r'^$',
+        TemplateView.as_view(template_name="news.html"),
         name='news'),
     ]
--- ddweb/apps/news/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def newsLatest(request, **kwargs):
-    context = {}
-    return render(request, 'news.html', context)
