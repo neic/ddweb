291134aca931b11b1fc2d6a69936fd41f46c2a3e
neic <neic@neic.dk>
2023-04-20 10:13:55 +0200

    Shellcheck docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -c '\l'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\l'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
 
 >&2 echo "Postgres is up - continuing"
 
-if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
+if [ "$DJANGO_MANAGEPY_MIGRATE" = 'on' ]; then
     pipenv run ./manage.py migrate --noinput
 fi
 
-if [ "x$DJANGO_MANAGEPY_COLLECTSTATIC" = 'xon' ]; then
+if [ "$DJANGO_MANAGEPY_COLLECTSTATIC" = 'on' ]; then
     pipenv run ./manage.py collectstatic --noinput
 fi
 
